ac9629d00a9d9db07fa78c0123ddfe79a6a5ad48
adityak77 <akannan2@andrew.cmu.edu>
2022-05-04 15:48:06 -0400

    fixed a lot of bugs, correct performance on cycle and complex graphs

--- hash_with_replacement/parallel_hash.h
+++ hash_with_replacement/parallel_hash.h
@@ -46,77 +46,6 @@ HashTable *create_hash_table(int size) {
     return hash_table;
 }
 
-// num_nodes is size of node_list
-Node* insert_deduplicate(HashTable *hash_table, Node *node_list, int num_nodes, std::vector<heap<int>*> &open_set) {
-    // dedup_list_mask indicates which indices in node_list have been deduped
-    // initially all are valid
-    int *dedup_list_mask = (int *) malloc(sizeof(int) * num_nodes);
-    int num_dedup_nodes = num_nodes;
-    for (int i = 0; i < num_nodes; i++) {
-        dedup_list_mask[i] = 1;
-    }
-
-    // run for loop in parallel
-    for (int i = 0; i < num_nodes; i++) {
-        // insert in random priority queue
-        int ind = rand() % open_set.size();
-        push_heap(open_set[ind], node_list[i].id, node_list[i].f);
-
-        // check for unoccupied slots with different hash functions
-        int z = 0;
-
-        int ind0 = hash_fn1(node_list[i].id, hash_table->size);
-        int ind1 = hash_fn2(node_list[i].id, hash_table->size);
-
-        if (hash_table->table[ind0].id == node_list[i].id || hash_table->table[ind0].id == -1) {
-            z = 0;
-        } else if (hash_table->table[ind1].id == node_list[i].id || hash_table->table[ind1].id == -1) {
-            z = 1;
-        }
-
-        Node t = node_list[i];
-
-        // START ATOMIC
-        Node temp = t;
-        if (z == 0) {
-            t = hash_table->table[ind0];
-            hash_table->table[ind0] = temp;
-        } else if (z == 1) {
-            t = hash_table->table[ind1];
-            hash_table->table[ind1] = temp;
-        }
-        // END ATOMIC
-
-        if (t.id == node_list[i].id) {
-            dedup_list_mask[i] = 0;
-            num_dedup_nodes--;
-            continue;
-        }
-
-        if (z == 0 && hash_table->table[ind1].id == node_list[i].id) {
-            dedup_list_mask[i] = 0;
-            num_dedup_nodes--;
-            continue;
-        } else if (z == 1 && hash_table->table[ind0].id == node_list[i].id) {
-            dedup_list_mask[i] = 0;
-            num_dedup_nodes--;
-            continue;
-        }
-    }
-
-    // create new list of deduped nodes
-    Node *dedup_list = (Node *) malloc(sizeof(Node) * num_dedup_nodes);
-    int j = 0;
-    for (int i = 0; i < num_nodes; i++) {
-        if (dedup_list_mask[i] == 1) {
-            dedup_list[j] = node_list[i];
-            j++;
-        }
-    }
-
-    return dedup_list;
-}
-
 // check if node exists in hash table and if it beats lowest g value
 // true if this node should be kept for insertion (i.e. either it's not in the table or new node's g is lower)
 // false otherwise (i.e. node is in the table and new node's g is higher)
@@ -151,4 +80,78 @@ Node query(HashTable *hash_table, int id) {
         // indicate node does not exist
         return Node(-1, -1, -1);
     }
+}
+
+// num_nodes is size of node_list
+Node* insert_deduplicate(HashTable *hash_table, Node *node_list, int num_nodes, std::vector<heap<int>*> &open_set) {
+    // dedup_list_mask indicates which indices in node_list have been deduped
+    // initially all are valid
+    int *dedup_list_mask = (int *) malloc(sizeof(int) * num_nodes);
+    int num_dedup_nodes = num_nodes;
+    for (int i = 0; i < num_nodes; i++) {
+        dedup_list_mask[i] = 1;
+    }
+
+    // run for loop in parallel
+    for (int i = 0; i < num_nodes; i++) {
+        // insert in random priority queue
+        int ind = rand() % open_set.size();
+        push_heap(open_set[ind], node_list[i].id, node_list[i].f);
+
+        // check for unoccupied slots with different hash functions
+        int z = 0;
+
+        int ind0 = hash_fn1(node_list[i].id, hash_table->size);
+        int ind1 = hash_fn2(node_list[i].id, hash_table->size);
+
+        if (hash_table->table[ind0].id == node_list[i].id || hash_table->table[ind0].id == -1) {
+            z = 0;
+        } else if (hash_table->table[ind1].id == node_list[i].id || hash_table->table[ind1].id == -1) {
+            z = 1;
+        }
+
+        Node t = node_list[i];
+
+        // START ATOMIC
+        // need to maintain that among the same nodes in node_list that the node
+        // with the smallest cost is the one that is the one that we keep
+        bool to_keep = query_cost_check(hash_table, t);
+        Node temp = t;
+        if (z == 0 && to_keep) {
+            t = hash_table->table[ind0];
+            hash_table->table[ind0] = temp;
+        } else if (z == 1 && to_keep) {
+            t = hash_table->table[ind1];
+            hash_table->table[ind1] = temp;
+        }
+        // END ATOMIC
+
+        if (t.id == node_list[i].id && to_keep) {
+            dedup_list_mask[i] = 0;
+            num_dedup_nodes--;
+            continue;
+        }
+
+        if (z == 0 && hash_table->table[ind1].id == node_list[i].id) {
+            dedup_list_mask[i] = 0;
+            num_dedup_nodes--;
+            continue;
+        } else if (z == 1 && hash_table->table[ind0].id == node_list[i].id) {
+            dedup_list_mask[i] = 0;
+            num_dedup_nodes--;
+            continue;
+        }
+    }
+
+    // create new list of deduped nodes
+    Node *dedup_list = (Node *) malloc(sizeof(Node) * num_dedup_nodes);
+    int j = 0;
+    for (int i = 0; i < num_nodes; i++) {
+        if (dedup_list_mask[i] == 1) {
+            dedup_list[j] = node_list[i];
+            j++;
+        }
+    }
+
+    return dedup_list;
 }
--- parallel_astar.cpp
+++ parallel_astar.cpp
@@ -11,15 +11,14 @@
 void print_heap(heap<int>* pq, int id) {
     printf("pq %d: ", id);
     for (int i = 0; i < pq->num_elements; ++i) {
-        printf("%d, ", pq->data[i].value);
+        printf("(%d, %f), ", pq->data[i].value, pq->data[i].priority);
     }
     printf("\n");
 }
 
 void print_node_vector(std::vector<Node> S) {
-    printf("S: ");
     for (Node n : S) {
-        printf("%d ", n.id);
+         printf("(%d, %d, %d, %d) ", n.id, n.prev_id, n.g, n.f);
     }
     printf("\n");
 }
@@ -100,7 +99,7 @@ std::vector<int> parallel_astar(
     int start,
     bool (*is_goal)(int),
     std::vector<link<int>> (*get_next)(int),
-    double (*f)(int), // f is not heuristic -- it is heuristic + g
+    double (*heuristic)(int), // f is not heuristic -- it is heuristic + g
     int num_queues
 ) {
     if (num_queues < 1) {
@@ -116,11 +115,12 @@ std::vector<int> parallel_astar(
     HashTable *closed_set = create_hash_table(20);
 
     // Initialize data structures
-    Node begin = Node(start, 0, f(start));
+    Node begin = Node(start, 0, heuristic(start));
     Node init_node_list[] = {begin};
     insert_deduplicate(closed_set, init_node_list, 1, open_set);
 
     int m = -1;
+    int fm = -1;
 
     // If all queues are empty, then failed to find goal state
     bool all_empty = true;
@@ -129,7 +129,6 @@ std::vector<int> parallel_astar(
         all_empty = all_empty && is_empty_heap(open_set[i]);
     }
 
-    // int cntr = 0;
     while (!all_empty) {
         std::vector<Node> S;
 
@@ -140,37 +139,50 @@ std::vector<int> parallel_astar(
             if (is_empty_heap(open_set[i])) continue;
 
             element<int> q = peak_heap(open_set[i])[0];
-            printf("q: %d\n", q.value);
             pop_heap(open_set[i]);
             int node = q.value;
             if (is_goal(node)) {
                 printf("reached a goal state!\n");
-                if (m == -1 || f(node) < f(m)) {
+                // ATOMIC REGION (?)
+                int fnode = q.priority;
+                if (m == -1 || fnode < fm) {
                     m = node;
+                    fm = fnode;
                 }
+                // END ATOMIC REGION (?)
                 continue;
             }
 
             for (link<int> neighbor : get_next(node)) {
                 assert(query(closed_set, node).g != -1);
                 int cost = query(closed_set, node).g + neighbor.cost;
-                Node new_node = Node(neighbor.node, node, cost, f(neighbor.node));
+                Node new_node = Node(neighbor.node, node, cost, cost+heuristic(neighbor.node));
                 S.push_back(new_node);
             }
         }
 
+        printf("S: ");
         print_node_vector(S);
 
         // return best path if goal is found and there is no
         // element in any of the pq's that are less than current
         // path to goal.
         if (m != -1) {
+            printf("f(m): %d\n", fm);
             bool all_less = true;
+
+            for (Node n : S) {
+                if (n.f < fm) {
+                    all_less = false;
+                    break;
+                }
+            }
+
             for (int i = 0; i < num_queues; i++) {
                 if (is_empty_heap(open_set[i])) continue;
 
-                int min_cost = peak_heap(open_set[i])->priority;
-                if (f(m) < min_cost) {
+                int min_cost = peak_heap(open_set[i])[0].priority;
+                if (min_cost < fm) {
                     all_less = false;
                     break;
                 }
@@ -195,20 +207,25 @@ std::vector<int> parallel_astar(
         bool query_mask[S.size()];
         int num_left = S.size();
         // run in parallel
-        for (int i = 0; i < S.size(); i++) {
+        for (size_t i = 0; i < S.size(); i++) {
             query_mask[i] = query_cost_check(closed_set, S[i]);
             if (!query_mask[i]) num_left--;
         }
 
         Node *T = (Node *) malloc(num_left * sizeof(Node));
         int j = 0;
-        for (int i = 0; i < S.size(); i++) {
+        for (size_t i = 0; i < S.size(); i++) {
             if (query_mask[i]) {
                 T[j] = S[i];
                 j++;
             }
         }
 
+        printf("T: ");
+        for (int i = 0; i < num_left; i++) {
+            printf("(%d, %d, %d, %d) ", T[i].id, T[i].prev_id, T[i].g, T[i].f);
+        }
+        printf("\n");
 
         // insert the remaining nodes in parallel in closed array
         // and priority queues.
@@ -220,8 +237,13 @@ std::vector<int> parallel_astar(
             all_empty = all_empty && is_empty_heap(open_set[i]);
         }
 
-        // cntr++;
-        // if (cntr > 5) throw "end early";
+        printf("Closed set\n");
+        for (int i = 0; i < closed_set->size; i++) {
+            Node n = closed_set->table[i];
+            printf("id: %d\t g: %d\t f: %d\n", n.id, n.g, n.f);
+        }
+        printf("\n");
+
         printf("-----NEW ITERATION-----\n");
     }
 
@@ -232,14 +254,160 @@ bool is_goal(int node) {
     return node == 10;
 }
 
+bool is_goal9(int node) {
+    return node == 9;
+}
+
+bool is_goal6(int node) {
+    return node == 6;
+}
+
 std::vector<link<int>> get_next(int node) {
     std::vector<link<int>> neighbors;
-    neighbors.push_back(link<int>(node + 1, node));
+    if (node > 0) {
+        neighbors.push_back(link<int>(node-1, node));
+    }
+    neighbors.push_back(link<int>(node + 1, node+1));
+    return neighbors;
+}
+
+std::vector<link<int>> get_next_cycle(int node) {
+    std::vector<link<int>> neighbors;
+    if (node > 0) {
+        neighbors.push_back(link<int>(node-1, node));
+    } else {
+        // large cost for skipping from 0 to 10
+        neighbors.push_back(link<int>(10, 1000));
+    }
+    neighbors.push_back(link<int>(node + 1, node+1));
+    return neighbors;
+}
+
+std::vector<link<int>> get_next_cycle2(int node) {
+    std::vector<link<int>> neighbors;
+    if (node > 0) {
+        neighbors.push_back(link<int>(node-1, node));
+    } else {
+        // small cost for skipping from 0 to 10
+        neighbors.push_back(link<int>(10, 37));
+    }
+    neighbors.push_back(link<int>(node + 1, node+1));
+    return neighbors;
+}
+
+// return trajectory changes via node 8
+std::vector<link<int>> get_next_cycle3(int node) {
+    std::vector<link<int>> neighbors;
+    if (node > 0) {
+        neighbors.push_back(link<int>(node-1, node));
+    } else {
+        // small cost for skipping from 0 to 10
+        neighbors.push_back(link<int>(10, 37));
+    }
+
+    if (node == 1) neighbors.push_back(link<int>(8, 33));
+
+    neighbors.push_back(link<int>(node + 1, node+1));
+    return neighbors;
+}
+
+// stolen from 16-311 HW 6 : pretty direct
+std::vector<link<int>> get_next_complex(int node) {
+    std::vector<link<int>> neighbors;
+    if (node == 0) {
+        neighbors.push_back(link<int>(1, 10));
+        neighbors.push_back(link<int>(3, 15));
+        neighbors.push_back(link<int>(4, 20));
+    } else if (node == 1) {
+        neighbors.push_back(link<int>(2, 15));
+        neighbors.push_back(link<int>(3, 5));
+    } else if (node == 2) {
+        neighbors.push_back(link<int>(3, 5));
+        neighbors.push_back(link<int>(6, 5));
+    } else if (node == 3) {
+        neighbors.push_back(link<int>(6, 10));
+    } else if (node == 4) {
+        neighbors.push_back(link<int>(3, 20));
+        neighbors.push_back(link<int>(5, 5));
+        neighbors.push_back(link<int>(8, 5));
+    } else if (node == 5) {
+        neighbors.push_back(link<int>(3, 10));
+        neighbors.push_back(link<int>(6, 20));
+        neighbors.push_back(link<int>(7, 20));
+        neighbors.push_back(link<int>(8, 15));
+        neighbors.push_back(link<int>(9, 5));
+    } else if (node == 6) {
+        neighbors.push_back(link<int>(7, 10));
+    } else if (node == 7) {
+        neighbors.push_back(link<int>(9, 5));
+    } else if (node == 8) {
+        neighbors.push_back(link<int>(9, 10));
+    }
+    return neighbors;
+}
+
+// also from 16-311: lots of backtracking
+std::vector<link<int>> get_next_complex2(int node) {
+    std::vector<link<int>> neighbors;
+    if (node == 0) {
+        neighbors.push_back(link<int>(1, 2));
+        neighbors.push_back(link<int>(3, 6));
+        neighbors.push_back(link<int>(2, 4));
+    } else if (node == 1) {
+        neighbors.push_back(link<int>(4, 2));
+    } else if (node == 2) {
+        neighbors.push_back(link<int>(5, 1));
+    } else if (node == 3) {
+        neighbors.push_back(link<int>(6, 11));
+    } else if (node == 4) {
+        neighbors.push_back(link<int>(3, 1));
+        neighbors.push_back(link<int>(6, 6));
+    } else if (node == 5) {
+        neighbors.push_back(link<int>(6, 6));
+    }
     return neighbors;
 }
 
 double heuristic(int node) {
     return 10 - node;
+}
+
+double heuristic9(int node) {
+    if (node > 9) {
+        return node-9;
+    }
+    return 9 - node;
+}
+
+// stolen from 16-311 HW 6
+double heuristic_complex(int node) {
+    if (node == 0) return 1000;
+    else if (node == 1) return 25;
+    else if (node == 2) return 15;
+    else if (node == 3) return 20;
+    else if (node == 4) return 10;
+    else if (node == 5) return 5;
+    else if (node == 6) return 10;
+    else if (node == 7) return 5;
+    else if (node == 8) return 5;
+    else if (node == 9) return 0;
+    else {
+        throw "You shouldn't be here";
+    }
+}
+
+// stolen from 16-311 HW 6
+double heuristic_complex2(int node) {
+    if (node == 0) return 1000;
+    else if (node == 1) return 1;
+    else if (node == 2) return 4;
+    else if (node == 3) return 9;
+    else if (node == 4) return 3;
+    else if (node == 5) return 1;
+    else if (node == 6) return 0;
+    else {
+        throw "You shouldn't be here";
+    }
 }
 
 void test_seq() {
@@ -255,7 +423,30 @@ void test_seq() {
 void test_par() {
     int start = 0;
     int num_queues = 5;
-    std::vector<int> ans = parallel_astar(start, is_goal, get_next, heuristic, num_queues);
+
+    // line graph
+    // cost = 55, path = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    // std::vector<int> ans = parallel_astar(start, is_goal, get_next, heuristic, num_queues);
+
+
+    // cycle graphs
+    // cost = 55, path = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    // std::vector<int> ans = parallel_astar(start, is_goal, get_next_cycle, heuristic, num_queues);
+    
+    // cost = 37, path = 0, 10
+    // std::vector<int> ans = parallel_astar(start, is_goal, get_next_cycle2, heuristic, num_queues);
+    
+    // cost = 45, path = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    // std::vector<int> ans = parallel_astar(start, is_goal9, get_next_cycle2, heuristic9, num_queues);
+    
+    // cost = 43, path = 0, 1, 8, 9
+    // std::vector<int> ans = parallel_astar(start, is_goal9, get_next_cycle3, heuristic9, num_queues);
+
+    // more complex graphs
+    // std::vector<int> ans = parallel_astar(start, is_goal9, get_next_complex, heuristic_complex, num_queues);
+    std::vector<int> ans = parallel_astar(start, is_goal6, get_next_complex2, heuristic_complex2, num_queues);
+
+    printf("returned trajectory: ");
     for (auto elem : ans) {
         printf("%d ", elem);
     }
